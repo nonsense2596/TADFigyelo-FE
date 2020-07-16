b4df1304625854e3dc11ed47975bb0447e4043f5
@nonsense2596 <peter.szatmary@windowslive.com>
2020-07-17 00:35:09 +0200

    getting original file version of the TADs

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
+    // TODO az opendir-t kicserélni a tényleges dirre, ahol majd a fileok lesznek
+    // akár változóba menteni, mert sokszor használom
     function ListItems() {
         if ($handle = opendir('.')) {
             while (false !== ($entry = readdir($handle))) {
                 if ($entry != "." && $entry != "..") {
-                    if(strpos($entry,"diff")!=false){
+                    if(strpos($entry,"diff")!==false){ // azaz ha true, azaz van benne diff
                         //////
+                        ///  kiirandó név meghatározása
                         $doc = new DOMDocument();
                         @$doc->loadHTML(@file_get_contents($entry));
                         $titlelist = $doc->getElementsByTagName("title");
+                        $title = null;
                         if($titlelist->length > 0){
-                            echo $titlelist->item(0)->nodeValue;
+                            $title = substr($titlelist->item(0)->nodeValue,12); // magic number
                         }
-
                         //////
-                        echo "<li><a href='$entry'>$entry</a></li>";
+                        /// itt megfogni a nem diff-es filet, és azt is feldolgozni
+                        /// TODO az összeset vagy legújabbat vagy valamit megfogni ebből szépen szisztematikusan
+                        if($handle2 = opendir('.')){
+                            while (false !== ($entry2 = readdir($handle2))) {
+                                if ($entry2 != "." && $entry2 != "..") {
+
+                                    $parts = explode('.',$entry);
+                                    //echo $parts[0].'<br>';
+
+                                    if(strpos($entry2,$parts[0])!==false && strpos($entry2,'_')!==false){
+                                        echo $entry2.'<br>';
+                                    }
+                                }
+                            }
+                        }
+                        //////
+                        // TODO originál verzió kész
+                        $parts = explode('.',$entry);
+                        $original = $parts[0].'.html';
+                        echo "<a href='$original'>$original</a><br>";
+
+
+
+                        echo "<li class='list-group-item'><a href='$entry'>$title</a></li>";
                     }
 
                 }
@@ -25,10 +51,11 @@
 <html lang="en">
 <head>
     <title>placeholder</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
-<h1>lol</h1>
-<ul>
+<h1>G<i>i</i><b>[</b>T<b>]</b>AD</h1>
+<ul class="list-group-flush">
     <?php ListItems(); ?>
 </ul>
 </body>
