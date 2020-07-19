0ceeb0cbadc36a684d619549eff6ef1219193e60
@nonsense2596 <peter.szatmary@windowslive.com>
2020-07-19 16:36:10 +0200

    design and order changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 if ($entry != "." && $entry != "..") {
                     if(strpos($entry,"diff")!==false){ // azaz ha true, azaz van benne diff
                         //////
-                        ///  kiirandó név meghatározása
+                        ///  kiirandó név meghatározása diff file mutatása
                         $doc = new DOMDocument();
                         @$doc->loadHTML(@file_get_contents($entry));
                         $titlelist = $doc->getElementsByTagName("title");
@@ -15,6 +15,19 @@
                         if($titlelist->length > 0){
                             $title = substr($titlelist->item(0)->nodeValue,12); // magic number
                         }
+
+                        echo "<div class='col-sm-6 mb-4'><div class='card'>";
+                        echo "<h5 class='card-header bg-primary text-white'><a class='text-white' href='$entry'>$title</a></h5>";
+                        //echo "<li class='list-group-item'><a href='$entry'>$title</a><br>";
+                        echo "<div class='card-body'>";
+
+                        // TODO originál verzió kész
+                        $parts = explode('.',$entry);
+                        $original = $parts[0].'.html';
+                        echo "<a class='card-text' href='$original'>Original: $original</a><br>";
+
+
+
                         //////
                         /// itt megfogni a nem diff-es filet, és azt is feldolgozni
                         /// TODO az összeset vagy legújabbat vagy valamit megfogni ebből szépen szisztematikusan
@@ -23,23 +36,24 @@
                                 if ($entry2 != "." && $entry2 != "..") {
 
                                     $parts = explode('.',$entry);
-                                    //echo $parts[0].'<br>';
 
                                     if(strpos($entry2,$parts[0])!==false && strpos($entry2,'_')!==false){
-                                        echo $entry2.'<br>';
+                                        //echo $entry2.'<br>'; ez KELL
+                                        // itt akkor mar tudjuk az adott fileok datumat
+                                        if(file_exists($entry2)){
+                                            echo "<a class='card-text' href='$entry2'>$entry2</a> was last modified: ".date("F d Y H:i:s.", filemtime($entry2))."<br>";
+                                        }
                                     }
                                 }
                             }
                         }
                         //////
-                        // TODO originál verzió kész
-                        $parts = explode('.',$entry);
-                        $original = $parts[0].'.html';
-                        echo "<a href='$original'>$original</a><br>";
 
 
-
-                        echo "<li class='list-group-item'><a href='$entry'>$title</a></li>";
+                        echo "</div>";
+                        //echo "</li>";
+                        //echo "<li class='list-group-item'><a href='$entry'>$title</a></li>";
+                        echo "</div></div>";
                     }
 
                 }
@@ -56,7 +70,15 @@
 <body>
 <h1>G<i>i</i><b>[</b>T<b>]</b>AD</h1>
 <ul class="list-group-flush">
-    <?php ListItems(); ?>
+    <?php /*ListItems();*/ ?>
 </ul>
+
+<div class="container">
+    <div class="row">
+        <?php ListItems(); ?>
+    </div>
+</div>
+
+
 </body>
 </html>
