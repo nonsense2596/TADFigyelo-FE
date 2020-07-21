e7b797a5d14b8245509460716d6932ff8379f416
@nonsense2596 <peter.szatmary@windowslive.com>
2020-07-21 13:07:56 +0200

    daily improvements

--- index.php
+++ index.php
@@ -5,52 +5,45 @@
         if ($handle = opendir('.')) {
             while (false !== ($entry = readdir($handle))) {
                 if ($entry != "." && $entry != "..") {
-                    if(strpos($entry,"diff")!==false){
+                    if(strpos($entry,"diff")!==false){  // igy csak azokat mutattja amit érdemes, amiből van diff file
                         //////
                         ///  kiirandó név meghatározása diff file mutatása
                         $doc = new DOMDocument();
-                        @$doc->loadHTML(@file_get_contents($entry));
+                        $parts = explode('.',$entry);
+                        @$doc->loadHTML(@file_get_contents($parts[0].'.'.$parts[2]));   // ne! a diff filet nezzuk az eredeti nev utan
                         $titlelist = $doc->getElementsByTagName("title");
-                        $title = null;
+                        $title = "";
                         if($titlelist->length > 0){
-                            $title = substr($titlelist->item(0)->nodeValue,12); // magic number
+                            $title = substr($titlelist->item(0)->nodeValue,10); // magic number
                         }
 
 
-                        echo "<div class='col-sm-6 mb-4'><div class='card'>";
-                        echo "<h5 class='card-header bg-primary text-white'>$title</h5>";
-                        echo "<div class='card-body'>";
 
+                        // originál verzió kész
 
-                        // TODO originál verzió kész
-                        $parts = explode('.',$entry);
                         $original = $parts[0].'.html';
 
 
-
-                        echo "<h6 class='card-text'><a class='card-text' href='$original'>Original: $original </a></h6><hr class='mt-3 mb-1'>";
-
-
-
+                        // diff verziok kilistazasa egyesevel
+                        $diffprint = "";
                         if($handle2 = opendir('.')){
                             while (false !== ($entry2 = readdir($handle2))) {
                                 if ($entry2 != "." && $entry2 != "..") {
-
                                     $parts = explode('.',$entry);
-
                                     if(strpos($entry2,$parts[0])!==false && strpos($entry2,'_')!==false){
-                                        //echo $entry2.'<br>'; ez KELL
-                                        // itt akkor mar tudjuk az adott fileok datumat
                                         if(file_exists($entry2)){
-                                            echo "<a class='card-text' href='$entry2'>$entry2</a> was last modified: ".date("F d Y H:i:s.", filemtime($entry2))."<hr class='my-1'>";
+                                            $diffprint = $diffprint."<a class='card-text' href='$entry2'>$entry2</a> was last modified: ".date("F d Y H:i:s.", filemtime($entry2))."<hr class='my-1'>";
                                         }
                                     }
                                 }
                             }
                         }
 
-
-
+                        echo "<div class='col-sm-6 mb-4'><div class='card'>";
+                        echo "<h5 class='card-header bg-primary text-white'>$title</h5>";
+                        echo "<div class='card-body'>";
+                        echo "<h6 class='card-text'><a class='card-text' href='$original'>Original: $original </a></h6><hr class='mt-3 mb-1'>";
+                        echo $diffprint;
                         echo "</div>";
                         echo "<div class='card-footer'><a href='$entry'>Show latest diff</a></div>";
                         echo "</div></div>";
@@ -75,7 +68,7 @@
 <div class="container">
     <h1>G<i>i</i><b>[</b>T<b>]</b>AD</h1>
     <div class="row mt-4">
-        <?php ListItems(); ?>
+        <?php ListItems(1); ?>
     </div>
 </div>
 
